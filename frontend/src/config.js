7b85a6e3f8f90fa99113baa058280d66ff9f0139
Gopika M <gopikamuru47@gmail.com>
2025-11-03 15:45:23 +0530

    updated files

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,6 +2,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "http://sameep-20-env.eba-ghup7zhc.ap-south-1.elasticbeanstalk.com/";
+    : "https://Sameep-V3.ap-south-1.elasticbeanstalk.com";
 
 export default API_BASE_URL;
